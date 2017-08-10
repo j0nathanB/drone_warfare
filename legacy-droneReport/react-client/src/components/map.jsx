610107a02dfb523129458a17c2ea5960429798a9
Jonathan Bradshaw <lavin@posteo.de>
2017-08-10 16:12:12 -0700

    Display map

--- legacy-droneReport/react-client/src/components/map.jsx
+++ legacy-droneReport/react-client/src/components/map.jsx
@@ -1,34 +1,20 @@
 import React from 'react';
 
-class Map extends React.Component {
-  constructor(props) {
-    super(props);
-    
-    this.state = {
-    }
+const EIFFEL_TOWER_POSITION = {
+  lat: 48.858608,
+  lng: 2.294471
+};
 
-    this.loadJS = this.loadJS.bind(this);
+class Map extends React.Component {
+  constructor() {
+    super();
   }
 
   componentDidMount() {
-    // Connect the initMap() function within this class to the global window context,
-    // so Google Maps can invoke it
-    window.initMap = this.initMap;
-    // Asynchronously load the Google Maps script, passing in the callback reference
-    loadJS('https://maps.googleapis.com/maps/api/js?key=AIzaSyAWh923QwLcLQGjH1w4OYOG0_CX8jGHbmE&callback=initMap')
-  }
-
-  initMap() {
-    map = new google.maps.Map(this.refs.map.getDOMNode());
-  }
-
-  loadJS(src) {
-    console.log("loaded")
-    let ref = window.document.getElementsByTagName("script")[0];
-    let script = window.document.createElement("script");
-    script.src = src;
-    script.async = true;
-    ref.parentNode.insertBefore(script, ref);
+    this.map = new google.maps.Map(this.refs.map, {
+      center: EIFFEL_TOWER_POSITION,
+      zoom: 16
+    });
   }
 
   render() {
