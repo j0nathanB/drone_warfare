7404e1ba80e6c684e0d454e95b6f2c7ea755ca77
Jonathan Bradshaw <j0nathanb.dev@gmail.com>
2023-03-29 15:32:42 -0700

    Change baseURL for github pages

--- src/geojsonHandler.js
+++ src/geojsonHandler.js
@@ -9,23 +9,30 @@ export class GeoJSONHandler {
   async loadData() {
     const results = {'AFG': [], 'PAK': [], 'SOM': [], 'YEM': []}; // Initialize an empty object to store the results
 
-    results['AFG'][0] = await loadJSON("../src/geojson/AFG_Adm_0.geojson");
-    results['AFG'][1] = await loadJSON("../src/geojson/AFG_Adm_1.geojson");
-    results['AFG'][2] = await loadJSON("../src/geojson/AFG_Adm_2.geojson");
-    results['AFG'][3] = await loadJSON("../src/geojson/AFG_Loc.geojson");
-    results['PAK'][0] = await loadJSON("../src/geojson/PAK_Adm_0.geojson");
-    results['PAK'][1] = await loadJSON("../src/geojson/PAK_Adm_1.geojson");
-    results['PAK'][2] = await loadJSON("../src/geojson/PAK_Adm_2.geojson");
-    results['PAK'][3] = await loadJSON("../src/geojson/PAK_Adm_3.geojson");
-    results['PAK'][4] = await loadJSON("../src/geojson/PAK_Loc.geojson");
-    results['SOM'][0] = await loadJSON("../src/geojson/SOM_Adm_0.geojson");
-    results['SOM'][1] = await loadJSON("../src/geojson/SOM_Adm_1.geojson");
-    results['SOM'][2] = await loadJSON("../src/geojson/SOM_Adm_2.geojson");
-    results['SOM'][3] = await loadJSON("../src/geojson/SOM_Adm_2.geojson");
-    results['YEM'][0] = await loadJSON("../src/geojson/YEM_Adm_0.geojson");
-    results['YEM'][1] = await loadJSON("../src/geojson/YEM_Adm_1.geojson");
-    results['YEM'][2] = await loadJSON("../src/geojson/YEM_Adm_2.geojson");
-    results['YEM'][3] = await loadJSON("../src/geojson/YEM_Loc.geojson");
+    const username = "j0nathanb";
+    const repo = "drone_warfare";
+    const ref = "main"; // usually "main" or "master"
+    const baseURL = `https://embed.github.com/view/geojson/${username}/${repo}/${ref}/src/geojson`;
+    // use the following line instead if you want to load the files from your local machine
+    // const baseURL = '../src/geojson'
+
+    results['AFG'][0] = await loadJSON(`${baseURL}/AFG_Adm_0.geojson`);
+    results['AFG'][1] = await loadJSON(`${baseURL}/AFG_Adm_1.geojson`);
+    results['AFG'][2] = await loadJSON(`${baseURL}/AFG_Adm_2.geojson`);
+    results['AFG'][3] = await loadJSON(`${baseURL}/AFG_Loc.geojson`);
+    results['PAK'][0] = await loadJSON(`${baseURL}/PAK_Adm_0.geojson`);
+    results['PAK'][1] = await loadJSON(`${baseURL}/PAK_Adm_1.geojson`);
+    results['PAK'][2] = await loadJSON(`${baseURL}/PAK_Adm_2.geojson`);
+    results['PAK'][3] = await loadJSON(`${baseURL}/PAK_Adm_3.geojson`);
+    results['PAK'][4] = await loadJSON(`${baseURL}/PAK_Loc.geojson`);
+    results['SOM'][0] = await loadJSON(`${baseURL}/SOM_Adm_0.geojson`);
+    results['SOM'][1] = await loadJSON(`${baseURL}/SOM_Adm_1.geojson`);
+    results['SOM'][2] = await loadJSON(`${baseURL}/SOM_Adm_2.geojson`);
+    results['SOM'][3] = await loadJSON(`${baseURL}/SOM_Adm_2.geojson`);
+    results['YEM'][0] = await loadJSON(`${baseURL}/YEM_Adm_0.geojson`);
+    results['YEM'][1] = await loadJSON(`${baseURL}/YEM_Adm_1.geojson`);
+    results['YEM'][2] = await loadJSON(`${baseURL}/YEM_Adm_2.geojson`);
+    results['YEM'][3] = await loadJSON(`${baseURL}/YEM_Loc.geojson`);
   
     return results
   }
